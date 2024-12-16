fcb3830090e6296df6baaf6fe7e4c2b7a4e6e32f
chikako ooshiro <e245715@ie.u-ryukyu.ac.jp>
2024-12-16 12:46:27 +0900

    2nd commit

--- src/jp/ac/uryukyu/ie/e245715/Report5.java
+++ src/jp/ac/uryukyu/ie/e245715/Report5.java
@@ -4,11 +4,25 @@ class Report5 {
 
     public static void main(String[] args){
 
-        //条件１ String型変数 strを宣言し初期値としてnullを代入
+        /*条件１ String型変数 strを宣言し初期値としてnullを代入
         String str = null;
 
         //条件２ str.lengh()の内容を表示しようとする
-        System.out.println(str.length());
+        System.out.println(str.length()); */
+
+        try{
+            //処理したいコード
+            String str = null;
+            System.out.println(str.length());
+
+        }catch(NullPointerException e){
+            //NullPointerExceptionが起きた時の処理
+            System.out.println(e.getMessage());
+
+
+        }
 
     }
+
+    
 }
