445d2bc24d6914b90066fbdf202155f649ef6f83
chikako ooshiro <e245715@ie.u-ryukyu.ac.jp>
2024-12-16 12:25:32 +0900

    1st commit

---
+++ src/jp/ac/uryukyu/ie/e245715/Report5.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.e245715;
+
+class Report5 {
+
+    public static void main(String[] args){
+
+        //条件１ String型変数 strを宣言し初期値としてnullを代入
+        String str = null;
+
+        //条件２ str.lengh()の内容を表示しようとする
+        System.out.println(str.length());
+
+    }
+}
